214334a2bbd5208d8908e4541b2f05dd03069938
ShusukeYoda <shusukeyoda@gmail.com>
2021-12-09 23:13:32 +0900

    prototypeRPG:windowsビルド前

--- Assets/Scripts/All.cs
+++ Assets/Scripts/All.cs
@@ -6,7 +6,6 @@ using System.IO;
 using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using TMPro;
 using UnityEditor;
 using UnityEngine;
--- Assets/Scripts/TextMethPro.cs
+++ Assets/Scripts/TextMethPro.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System.IO;
-using System.Windows.Forms;
 
 public class TextMethPro : MonoBehaviour
 {
