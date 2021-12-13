8c9def56f8e4762b74f2b0b402f87713955da247
0808uni <88703858+0808uni@users.noreply.github.com>
2021-12-13 20:42:23 +0900

    微整理

--- Assets/Scripts/pullDown.cs
+++ Assets/Scripts/pullDown.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
-using Button = UnityEngine.UI.Button;
 
 public class pullDown : MonoBehaviour
 {
