b7b82d76dae8e06789a2dc45657d516a50b7a1cb
0808uni <88703858+0808uni@users.noreply.github.com>
2021-12-13 19:47:45 +0900

    Button3追加セーブ

--- Assets/Scripts/Button3.cs
+++ Assets/Scripts/Button3.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 using Button = UnityEngine.UI.Button;
 using Image = UnityEngine.UI.Image;
 
