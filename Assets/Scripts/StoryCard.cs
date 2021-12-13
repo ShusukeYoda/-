6b8fe108cbf7259547b4dfc37f1aac288e68051b
0808uni <88703858+0808uni@users.noreply.github.com>
2021-12-13 19:52:29 +0900

    StoryCardクラス確認

--- Assets/Scripts/StoryCard.cs
+++ Assets/Scripts/StoryCard.cs
@@ -1,14 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StoryCard : MonoBehaviour
 {
     public List<Sprite> images;    //List½Ìß×²Ä
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 }
