2e7f7948002b86911bcc155ee3c997cdf5095cae
0808uni <88703858+0808uni@users.noreply.github.com>
2021-12-13 19:46:50 +0900

    Button1,2,3リファクタリング

--- Assets/Scripts/Button1.cs
+++ Assets/Scripts/Button1.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-using System.Threading.Tasks;
 using UnityEngine.UI;
 
 public class Button1 : MonoBehaviour
@@ -10,28 +6,16 @@ public class Button1 : MonoBehaviour
     GameObject card;
     GameObject SE9;
     GameObject SE10;
-    GameObject SE11;
-    GameObject storyCard;
-    GameObject TextTMP;
-    GameObject Audio1;
-    GameObject Audio3;
     All all;
     Tarot tarot;
-    StoryCard SCard;
 
     Button button;
     void Start()
     {
         this.card = GameObject.Find("card");
         this.tarot = GameObject.Find("card").GetComponent<Tarot>();
-        this.SCard = GameObject.Find("storyCard").GetComponent<StoryCard>();
         this.SE9 = GameObject.Find("shaffleSE");
         this.SE10 = GameObject.Find("tarot&StartSE");
-        this.SE11 = GameObject.Find("cardStopSE");
-        this.storyCard = GameObject.Find("storyCard");
-        this.TextTMP = GameObject.Find("TextTMP");
-        this.Audio1 = GameObject.Find("BGM");
-        this.Audio3 = GameObject.Find("BGM2");
         //クラスを取得する書き方
         this.all = GameObject.Find("Main Camera").GetComponent<All>();
 
@@ -75,43 +59,6 @@ public class Button1 : MonoBehaviour
             //Debug//絵柄はall.countだが能力は戦車
             //tarot.ChooseTarot(7); 
             tarot.ChooseTarot(all.count);
-        }
-    }
-
-
-
-    int random1;
-    public void StoryClick()
-    {
-        if (all.one && all.walk)
-        {
-            random1 = UnityEngine.Random.Range(1, 4);
-            //int random1 = 2;   //ストーリーcardデバッグ用
-            all.sNum += random1;
-
-
-            if (all.sNum <= 21 && all.walk)
-            {
-                SE10.GetComponent<AudioSource>().Play();
-                all.walk = false;
-                storyCard.GetComponent<Image>().sprite = SCard.images[all.sNum];
-
-                var textAsset = Resources.Load("image" + all.sNum) as TextAsset;
-                TextTMP.GetComponent<TextMeshProUGUI>().text = textAsset.ToString();
-            }
-            else if (all.sNum >= 22 && all.walk)
-            {
-                SE10.GetComponent<AudioSource>().Play();
-                Audio1.GetComponent<AudioSource>().Stop();
-                Audio3.GetComponent<AudioSource>().Play();
-
-                var textAsset = Resources.Load("EndImage") as TextAsset;
-                TextTMP.GetComponent<TextMeshProUGUI>().text = textAsset.ToString();
-
-                storyCard.GetComponent<Image>().sprite = SCard.images[23];
-
-                all.walk = false;
-            }
         }
     }
 }
--- Assets/Scripts/Button3.cs
+++ Assets/Scripts/Button3.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
+using Image = UnityEngine.UI.Image;
 
 public class Button3 : MonoBehaviour
 {
+    All all;
+    StoryCard SCard;
+    GameObject storyCard;
+    GameObject TextTMP;
+    GameObject SE10;
+    GameObject Audio1;
+    GameObject Audio3;
+    Button button;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        this.all = GameObject.Find("Main Camera").GetComponent<All>();
+        this.SCard = GameObject.Find("storyCard").GetComponent<StoryCard>();
+        this.storyCard = GameObject.Find("storyCard");
+        this.TextTMP = GameObject.Find("TextTMP");
+        this.SE10 = GameObject.Find("tarot&StartSE");
+        this.Audio1 = GameObject.Find("BGM");
+        this.Audio3 = GameObject.Find("BGM2");
 
-    // Update is called once per frame
-    void Update()
+        button = GetComponent<Button>();
+        //ボタンを押下したときのリスナーを設定
+        button.onClick.AddListener(() => StoryClick());
+    }
+    int random1;
+    public void StoryClick()
     {
-        
+        if (all.one && all.walk)
+        {
+            random1 = UnityEngine.Random.Range(1, 4);
+            //int random1 = 2;   //ストーリーcardデバッグ用
+            all.sNum += random1;
+
+            if (all.sNum <= 21 && all.walk)
+            {
+                SE10.GetComponent<AudioSource>().Play();
+                all.walk = false;
+                storyCard.GetComponent<Image>().sprite = SCard.images[all.sNum];
+
+                var textAsset = Resources.Load("image" + all.sNum) as TextAsset;
+                TextTMP.GetComponent<TextMeshProUGUI>().text = textAsset.ToString();
+            }
+            else if (all.sNum >= 22 && all.walk)
+            {
+                SE10.GetComponent<AudioSource>().Play();
+                Audio1.GetComponent<AudioSource>().Stop();
+                Audio3.GetComponent<AudioSource>().Play();
+
+                var textAsset = Resources.Load("EndImage") as TextAsset;
+                TextTMP.GetComponent<TextMeshProUGUI>().text = textAsset.ToString();
+
+                storyCard.GetComponent<Image>().sprite = SCard.images[23];
+
+                all.walk = false;
+            }
+        }
     }
 }
